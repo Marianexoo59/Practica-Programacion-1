8859c8525623b347215ed186ff92e225e31f7705
Mariano Aressi <marianoaressi1@gmail.com>
2025-06-23 21:33:31 -0300

    Ejercicios programacion unidad 8

---
+++ EjerciciosUnidad8/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Ejercicios;
+namespace EjerciciosUnidad8;
+internal class Program
+{
+   public static void Main(string[] args)
+    {
+
+        //EJERCICIO 1
+        //Animal animalito = new Perro();
+        //animalito.hacerSonido();
+
+        //EJERCICIO 2
+        //Direccion direccionsita = new Direccion("Balcarce", "Sunchales");
+        //Persona personita = new Persona("Mariano", 20, direccionsita);
+
+        //Console.WriteLine($"{personita.nombre}, tiene {personita.edad}, y vive en la calle {personita.datosDireccion.calle} en la ciudad de {personita.datosDireccion.ciudad}");
+
+        //EJERCICIO 3
+        //Empleado empleadito1 = new EmpleadoPermanente(5, 10);
+        //Empleado empleadito2 = new EmpleadoTemporal(5, 10);
+
+        //Console.WriteLine($"El empleado permanente {empleadito1.sueldo}, y el empleado temporal cobra {empleadito2.sueldo}");
+
+        //EJERCICIO 4
+        //{
+        //    List<IVehiculo> vehiculos = new List<IVehiculo>();
+
+        //    vehiculos.Add(new Auto());
+        //    vehiculos.Add(new Bicicleta());
+
+        //    foreach (var vehiculo in vehiculos)
+        //    {
+        //        vehiculo.Conducir();
+        //    }
+        //}
+
+        //EJERCICIO 5
+        //Curso curso = new Curso();
+
+        //curso.AgregarAlumno(new Alumno("Juan"));
+        //curso.AgregarAlumno(new Alumno("Ana"));
+        //curso.AgregarAlumno(new Alumno("Luis"));
+
+        //foreach (Alumno alumno in curso.Alumnos)
+        //{
+        //    Console.WriteLine(alumno.Nombre);
+        //}
+    }
+}
