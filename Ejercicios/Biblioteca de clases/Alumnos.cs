aec03c9967188a55baa530dfc7518caa8072cfcb
Mariano Aressi <marianoaressi1@gmail.com>
2025-06-24 00:52:48 -0300

    a

---
+++ Ejercicios/Biblioteca de clases/Alumnos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios
+{
+    public class Alumno
+    {
+        public string Nombre { get; set; }
+
+        public Alumno(string nombre)
+        {
+            Nombre = nombre;
+        }
+    }
+
+    public class Curso
+    {
+        public List<Alumno> Alumnos { get; set; } = new List<Alumno>();
+
+        public void AgregarAlumno(Alumno alumno)
+        {
+            Alumnos.Add(alumno);
+        }
+    }
+}
---
+++ Ejercicios/Biblioteca de clases/Animal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Ejercicios
+
+{
+        public class Animal()
+        {
+            public virtual void hacerSonido()
+            {
+                Console.WriteLine("*Hace un sonido*");
+            }
+        }
+
+        public class Perro : Animal
+        {
+            public override void hacerSonido()
+            {
+                Console.WriteLine("Guau");
+            }
+        }
+
+        public class Vaca : Animal
+        {
+            public override void hacerSonido()
+            {
+                Console.WriteLine("Muuuu");
+            }
+        }
+}
