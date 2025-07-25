a716a9d9403ab32f947f2921088db816e611702f
Mariano Aressi <marianoaressi1@gmail.com>
2025-07-25 20:55:09 -0300

    Nuevo ejercicio de partidos

---
+++ Ejercicios PROGRAMACION 1/Ejercicio entregable partidos/Equipo.cs
@@ -0,0 +1,102 @@
+﻿using ClasesApp;
+
+namespace ClasesApp
+{
+    public class Equipo
+    {
+        public string Nombre { get; set; }
+        public string Ciudad { get; set; }
+
+        public int Puntos { get; set; }
+        public int GolesAFavor { get; set; }
+        public int GolesEnContra { get; set; }
+
+        public int DiferenciaGoles
+        {
+            get { return GolesAFavor - GolesEnContra; }
+        }
+
+        public Equipo(string Nombre, string Ciudad)
+        {
+            this.Nombre = Nombre;
+            this.Ciudad = Ciudad;
+            Puntos = 0;
+            GolesAFavor = 0;
+            GolesEnContra = 0;
+        }
+
+        public void RegistrarResultado(Equipo local, Equipo visitante, int golesLocal, int golesVisitante)
+        {
+            local.GolesAFavor += golesLocal;
+            local.GolesEnContra += golesVisitante;
+
+            visitante.GolesAFavor += golesVisitante;
+            visitante.GolesEnContra += golesLocal;
+
+            if (golesLocal > golesVisitante)
+                local.Puntos += 3;
+            else if (golesLocal < golesVisitante)
+                visitante.Puntos += 3;
+            else
+            {
+                local.Puntos += 1;
+                visitante.Puntos += 1;
+            }
+        }
+    }
+    public class AdministradorTorneo : Partidos
+    {
+        List<Equipo> equipos = new List<Equipo>();
+
+        public void AgregarEquipo(string Nombre, string Ciudad)
+        {
+            equipos.Add(new Equipo(Nombre, Ciudad));
+        }
+
+        public List<Equipo> ObtenerEquipos()
+        {
+            return equipos;
+        }
+
+        public bool HayEquipos()
+        {
+            return equipos.Any();
+        }
+
+        public void JugarPartido(Equipo local, Equipo visitante, int golesLocal, int golesVisitante)
+        {
+            local.GolesAFavor += golesLocal;
+            local.GolesEnContra += golesVisitante;
+
+            visitante.GolesAFavor += golesVisitante;
+            visitante.GolesEnContra += golesLocal;
+
+            if (golesLocal > golesVisitante)
+                local.Puntos += 3;
+            else if (golesLocal < golesVisitante)
+                visitante.Puntos += 3;
+            else
+            {
+                local.Puntos += 1;
+                visitante.Puntos += 1;
+            }
+
+            historialPartidos.Add(new Partidos
+            {
+                Local = local,
+                Visitante = visitante,
+                GolesLocal = golesLocal,
+                GolesVisitante = golesVisitante,
+                Fecha = DateTime.Now
+            });
+        }
+
+        List<Partidos> historialPartidos = new List<Partidos>();
+
+        public List<Partidos> ObtenerHistorial()
+        {
+            return historialPartidos;
+        }
+    }
+}
+
---
+++ Ejercicios PROGRAMACION 1/Ejercicio entregable partidos/Partidos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesApp
+{
+    public class Partidos
+    {
+        public Equipo Local { get; set; }
+        public Equipo Visitante { get; set; }
+        public int GolesLocal { get; set; }
+        public int GolesVisitante { get; set; }
+        public DateTime Fecha { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Fecha.ToShortDateString()} - {Local.Nombre} {GolesLocal} : {GolesVisitante} {Visitante.Nombre}";
+        }
+    }
+}
