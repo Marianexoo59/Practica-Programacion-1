2cf5b7b03939ec4590f9165919c8e6175f7e8e97
Mariano Aressi <marianoaressi1@gmail.com>
2025-08-21 13:40:18 -0300

    Codigo Corregido y cambio de carpetas

---
+++ Ejercicios PROGRAMACION 1/Ejercicio Gestor de viajes/Biblioteca de clases/Camion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca_de_clases
+{
+    internal class Camion : Vehiculo
+    {
+        public int CargaAdicional { get; set; }
+
+        public Camion(int placa, int kilometraje, int viajes, int cargaAdicional, int Capacidad) 
+            : base(placa, kilometraje, viajes, Capacidad)
+        {
+            this.CargaAdicional = cargaAdicional;
+        }
+
+        public override void DistanciaTotal()
+        {
+            
+        }
+        public override void AgregarViaje()
+        {
+            
+        }
+
+        public void CargaMaxima(int carga)
+        {
+            Console.WriteLine($"La carga máxima del camión es {Capacidad + carga} toneladas");
+        }
+    }
+}
---
+++ Ejercicios PROGRAMACION 1/Ejercicio Gestor de viajes/Biblioteca de clases/Motocicleta.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca_de_clases
+{
+    internal class Motocicleta : Vehiculo
+    {
+        public Motocicleta(int Placa, int Kilometraje, int Viajes, int Capacidad) : base(Placa, Kilometraje, Viajes, Capacidad)
+        {
+            
+        }
+
+        public override void DistanciaTotal()
+        {
+            Kilometraje = Kilometraje;
+        }
+
+        public override void AgregarViaje()
+        {
+            
+        }
+    }
+}
