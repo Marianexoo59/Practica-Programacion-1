5e91eb63c03622bc13d566f8fab3ecac1ca76cb0
Marianexoo59 <marianoaressi1@gmail.com>
2025-10-01 20:47:56 -0300

    ??

--- Ejercicios Programacion I/EjercicioEntregable 17-9/ClassLibrary/obj/Debug/net8.0/ClassLibrary.AssemblyInfo.cs
+++ Ejercicios Programacion I/EjercicioEntregable 17-9/ClassLibrary/obj/Debug/net8.0/ClassLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86cb35a899e92ccb9dc9477edcadb521267541cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
